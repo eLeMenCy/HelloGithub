44265558e4b0c0768361526dc2c05ab214b94dff
eLeMenCy <dev@elemency.com>
2020-09-27 16:41:07 +0200

    Message modified again

--- src/main/java/com/elemency/NebulaTester/App.java
+++ src/main/java/com/elemency/NebulaTester/App.java
@@ -4,7 +4,7 @@ package com.elemency.NebulaTester;
 public class App {
 
     public static void main(String[] Args) {
-        System.out.println("hello mad World");
+        System.out.println("hello Nebula, pretty cool thing!");
         for (int i = 0; i < 10; i++) {
             System.out.println("i = " + ((i + 1) * 2 ));
         }
