9db934f11e58d9c22326a387350427e1f12e78fd
eLeMenCy <dev@elemency.com>
2020-09-27 16:55:57 +0200

    Message modified again and again and again and again and again

--- src/main/java/com/elemency/NebulaTester/App.java
+++ src/main/java/com/elemency/NebulaTester/App.java
@@ -4,7 +4,7 @@ package com.elemency.NebulaTester;
 public class App {
 
     public static void main(String[] Args) {
-        System.out.println("hello gi");
+        System.out.println("hello git");
         for (int i = 0; i < 10; i++) {
             System.out.println("i = " + ((i + 1) * 2 ));
         }
