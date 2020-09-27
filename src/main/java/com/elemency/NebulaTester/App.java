fa406432cdb4556f96b9189f037ffedad9402c7c
eLeMenCy <dev@elemency.com>
2020-09-27 14:51:26 +0200

    Changed message and added a counter

--- src/main/java/com/elemency/NebulaTester/App.java
+++ src/main/java/com/elemency/NebulaTester/App.java
@@ -2,7 +2,11 @@ package com.elemency.NebulaTester;
 
 
 public class App {
+
     public static void main(String[] Args) {
-        System.out.println("hello World");
+        System.out.println("hello new World");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + (i + 1));
+        }
     }
 }
